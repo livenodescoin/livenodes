bd25adaa127d8dfa27f9373ccb3f59a78b7a8d3f
Livenodes <livenodes@gmail.com>
2019-11-08 00:02:54 +0200

    Version and protocol bump

--- src/version.h
+++ src/version.h
@@ -13,7 +13,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 90022;
+static const int PROTOCOL_VERSION = 90023;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -24,8 +24,8 @@ static const int GETHEADERS_VERSION = 70077;
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 90010;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 90011;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_2 = 90021;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_3 = 90022;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_2 = 90023;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_3 = 90023;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
